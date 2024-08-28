2084bf5c4b6f9568819731e08be5ad0e8aecdecf
NathanV124 <ntverrill@ksu.edu>
2024-08-28 17:26:45 -0500

    Update CoordinateCalculator.py
    Finished the coordinate calculator in VS

--- CoordinateCalculator.py
+++ CoordinateCalculator.py
@@ -1,6 +1,20 @@
 ##-------------------------------------------------------------------------------------
 ## Name:  CoordinateCalculator.py
 ## Description:  Converts geographic coordinates in DDMMSS format to DD
-## Created:  June 15, 2023
-## Author:  Shawn Hutchinson (shutch@ksu.edu)
+## Created:  August 28th 2024
+## Author:  Nathan Verrill (ntverrill@ksu.edu)
 ##-------------------------------------------------------------------------------------
+
+coordinate = input("Enter the coordinates in DDMMSS format:")
+
+def convert_decimal(coord):
+    degrees = int(coord[:2])
+    minutes = int(coord[2:4])
+    seconds = int(coord[4:])
+
+    decimal_degrees = degrees + (minutes / 60) + (seconds / 3600)
+    return decimal_degrees
+
+decimal_degrees = convert_decimal(coordinate)
+print(f"Your final result is: {round(decimal_degrees, 2)} degrees")
+    
